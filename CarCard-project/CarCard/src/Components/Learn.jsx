008e0bd768cd6cb6550a86e9afcf3fa0cc66b1b1
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-06-15 16:23:22 +0330

    Add: some codes and styles to Learn.jsx

---
+++ CarCard-project/CarCard/src/Components/Learn.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+const Learn = ({Learn}) => {
+    return (
+        <div>
+            <a href="#"> <span className='learn'> {Learn} </span> </a>
+        </div>
+    )
+}
+
+export default Learn;
