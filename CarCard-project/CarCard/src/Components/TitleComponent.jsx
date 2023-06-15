2e727f699f1d1ca5a5f25cecd0d2a772252ea4d9
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-06-15 16:11:28 +0330

    Add: added image, css, TitleComponent.jsx

---
+++ CarCard-project/CarCard/src/Components/TitleComponent.jsx
@@ -0,0 +1,11 @@
+const TitleComponent = ({title}) => {
+    return (
+        <div>
+            <h1 className={'title'}>
+                {title}
+            </h1>
+        </div>
+    )
+}
+
+export default TitleComponent;
