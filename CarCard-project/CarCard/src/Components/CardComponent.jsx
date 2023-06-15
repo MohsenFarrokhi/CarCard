17602df26bd37298a50cb7e8912324aa9b4cd660
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-06-15 16:40:52 +0330

    Add: import components to CardComponent.jsx

--- CarCard-project/CarCard/src/Components/CardComponent.jsx
+++ CarCard-project/CarCard/src/Components/CardComponent.jsx
@@ -1,9 +1,31 @@
-const CardComponent = ({image,title,description,share,learn}) => {
-  return (
-      <div>
-          <div className="card-container">
+import ImageComponent from "./ImageComponent.jsx";
+import TitleComponent from "./TitleComponent.jsx";
+import Description from "./Description.jsx";
+import Share from "./Share.jsx";
+import Learn from "./Learn.jsx";
 
-          </div>
-      </div>
-  )
+const CardComponent = ({image, title, description, share, learn}) => {
+    return (
+        <div>
+            <div className="card-container">
+                <div>
+                    <ImageComponent image={image}/>
+                </div>
+                <div>
+                    <div>
+                        <TitleComponent title={title}/>
+                    </div>
+                    <div>
+                        <Description description={description}/>
+                    </div>
+                    <div className='learn-share'>
+                        <Share share={share}/>
+                        <Learn Learn={learn}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default CardComponent
