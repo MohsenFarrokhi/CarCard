489fb6e7e27122fe89c091da3bb5a92462cb1374
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-06-15 17:40:02 +0330

    Add: added some codes and fix

--- CarCard-project/CarCard/src/App.jsx
+++ CarCard-project/CarCard/src/App.jsx
@@ -5,11 +5,11 @@ function App() {
     return (
         <div>
             <CardComponent
-                img={"CarCard-project/CarCard/public/File_000.png"}
-                title={"Lizard"}
-                description={"Chevrolet is an iconic American car brand known for its reliable,dependable,and affroadable vehicles. Founded in 1911,Chevy has become one of the most recognizable car brands in the world."}
-                share={'SHARE'}
-                learn={"LEARN MORE"}
+                image="../public/Car.jpg"
+                title="Lizard"
+                description="Chevrolet is an iconic American car brand known for its reliable,dependable,and affroadable vehicles. Founded in 1911,Chevy has become one of the most recognizable car brands in the world."
+                share='SHARE'
+                learn="LEARN MORE"
             />
         </div>
     )
